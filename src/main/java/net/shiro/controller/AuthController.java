aa874dd7b1e6f6881bbca6bb90ea59b687893048
gaoxinlei <xinleigao@yeah.net>
2017-09-24 23:07:55 +0800

    网页基础验证 hello world

--- src/main/java/net/shiro/controller/AuthController.java
+++ src/main/java/net/shiro/controller/AuthController.java
@@ -1,5 +1,10 @@
 package net.shiro.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
@@ -8,5 +13,18 @@ public class AuthController {
     @RequestMapping("/toTarget")
     public String toTarget() {
         return "target";
+    }
+    
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+        Subject subject = SecurityUtils.getSubject();
+        if(!subject.isAuthenticated()) {
+            System.out.println(token.hashCode());
+            subject.login(token);
+        }
+        return "redirect:/list.jsp";
     }
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,5 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,5 +8,10 @@
 </head>
 <body>
 <h1>login.jsp</h1>
+<form action="${pageContext.request.contextPath }/login" method="post">
+	用户名：<input type="text" name="username"><br>
+	密码：<input type="password" name="password"><br>
+	<input type="submit" value="提交">
+</form>
 </body>
 </html>
